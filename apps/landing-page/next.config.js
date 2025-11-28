e17b223176bf5813bbc225156a60374cc6b6ea4f
duxirosenberg <d.rosenberg48@gmail.com>
2025-11-28 17:15:11 +0000

    feat: add rewrite config for captable app proxy

--- apps/landing-page/next.config.js
+++ apps/landing-page/next.config.js
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: '/captable',
+        destination: `${process.env.CAPTABLE_APP_URL}/captable/`,
+      },
+      {
+        source: '/captable/:path*',
+        destination: `${process.env.CAPTABLE_APP_URL}/captable/:path*`,
+      },
+    ];
+  },
 }
 
 module.exports = nextConfig
